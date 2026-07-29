a3b8a8a11ac9698b107bd8161bbe281f6f8eab82
Bigboss298 <abdullahbisiriyu988@gmail.com>
2026-07-29 02:27:53 +0100

    fix

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { clearAuthToken, clearAuthTokenExpiry, getAuthToken } from './authToken'
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'http://Cbbs-env.eba-p4zbykdz.eu-west-1.elasticbeanstalk.com/'
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'https://cbbs.idealsmartsolutions.com/'
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
